b327f3b9f8610ae15575cc63f14316fce06435e3
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-15 23:07:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitolize!}:"
     holiday.each do |name|
-      name.
+      name.to_s
     
   end
       
